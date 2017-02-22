754e9c05b7e35d76b083b584eda59ad572e1df13
jsbeyond <67664518@qq.com>
2017-02-22 21:48:54 +0800

    kiwi: update for candy7.1

--- candy.mk
+++ candy.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Candy stuff.
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kiwi
+PRODUCT_NAME := candy_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
